35c02e50256d94d30a6e17cf2a2458ace137b438
Ricardo <ribardir00@gmail.com>
2026-01-03 17:09:27 -0600

    Página de login 7

--- includes/header.php
+++ includes/header.php
@@ -2,3 +2,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="shortcut icon" type="image/png" href="/evaldoc/assets/images/logos/favicon.png" />
 <link rel="stylesheet" href="/evaldoc/assets/css/styles.min.css" />
+<link rel="stylesheet" href="/evaldoc/assets/libs/fontawesome/css/fontawesome.css">
+<link rel="stylesheet" href="/evaldoc/assets/libs/fontawesome/css/solid.css">
